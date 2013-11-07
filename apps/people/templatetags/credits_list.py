db3a36adb745052f50640a5adf3d2e8a2f7a372a
Geoff Kimball <gakimball@bsu.edu>
2013-11-07 10:46:35 -0500

    Add template tag for album credits

---
+++ apps/people/templatetags/credits_list.py
@@ -0,0 +1,16 @@
+from django import template
+from apps.people.models import Credit
+from django.contrib.contenttypes.models import ContentType
+
+register = template.Library()
+
+@register.inclusion_tag('people/credits_list.html', takes_context=True)
+def credits_list(context, item):
+  content_type = ContentType.objects.get_for_model(item)
+  object_id    = item.id
+
+  credits_list = Credit.objects.filter(content_type=content_type, object_id=object_id).order_by('person')
+
+  return {
+  'credits_list': credits_list,
+  }
