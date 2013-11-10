52272c94751f82846620d34d70af7a7fc2f71245
Geoff Kimball <gakimball@bsu.edu>
2013-11-10 03:52:52 -0500

    Update credits list template tag

--- apps/people/templatetags/credits_list.py
+++ apps/people/templatetags/credits_list.py
@@ -9,7 +9,7 @@ def credits_list(context, item):
   content_type = ContentType.objects.get_for_model(item)
   object_id    = item.id
 
-  credits_list = Credit.objects.filter(content_type=content_type, object_id=object_id).order_by('person')
+  credits_list = Credit.objects.filter(content_type=content_type, object_id=object_id).order_by('person').prefetch_related('person')
 
   return {
   'credits_list': credits_list,
