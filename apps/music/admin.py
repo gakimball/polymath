e2d5c9fbe9387acf731ed867b12b83f8b5a31a16
Geoff Kimball <gakimball@bsu.edu>
2013-11-10 21:53:04 -0500

    Update admin interfaces for music models

--- apps/music/admin.py
+++ apps/music/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from apps.music.models import Track, Album, Artist
 
-admin.site.register(Track)
-admin.site.register(Album)
+def artists(obj):
+  return ', '.join(str(item) for item in obj.artists.all())
+
+def track_count(obj):
+  return obj.tracks.count()
+
+class TrackAdmin(admin.ModelAdmin):
+  list_display = ('title', 'artist', 'album', 'pub_date')
+  ordering = ('-pub_date',)
+
+class AlbumAdmin(admin.ModelAdmin):
+  list_display = ('title', artists, 'year', track_count)
+
+admin.site.register(Track, TrackAdmin)
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Artist)
