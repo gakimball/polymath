462844f437b73ced517aa2976a76220f5a372622
Geoff Kimball <gakimball@bsu.edu>
2013-11-18 19:38:38 -0500

    Add next/prev/playpause/playlist functionality to player

--- polymath/static/js/src/script.js
+++ polymath/static/js/src/script.js
@@ -10,11 +10,15 @@
       bPlayPause:   $obj.find('[data-playpause]'),
       bPrevTrack:   $obj.find('[data-prevtrack]'),
       bNextTrack:   $obj.find('[data-nexttrack]'),
+      bMenuButton:  $obj.find('[data-menu]'),
       uSlider:      $obj.find('[data-slider]'),
+      uPlaylist:    $obj.find('[data-playlist]'),
+      uProgress:    $obj.find('[data-progress]'),
       iTrackTitle:  $obj.find('[data-title]').eq(0),
       iArtistName:  $obj.find('[data-artist]').eq(0),
       iCurrentTime: $obj.find('[data-currenttime]').eq(0),
       iTotalTime:   $obj.find('[data-totaltime]').eq(0),
+      iCoverArt:    $obj.find('[data-coverart]'),
     };
 
     this.playlist = [];
@@ -29,10 +33,11 @@
   Player.prototype = {
     initialize: function() {
       this.initialized = true;
+      this.$player.removeClass('inactive is-playlist-open');
     },
 
     events: function() {
-      self = this;
+      var self = this;
 
       // Internal triggers
       this.$audio.on({
@@ -42,10 +47,17 @@
         'timeupdate': function() {
           self.updateTimeUI();
         },
+        'play': function() {
+          self.ui.bPlayPause.html('&#8214;');
+        },
+        'pause': function() {
+          self.ui.bPlayPause.html('&#9654;');
+        }
       });
       this.$player.on({
         'player-playlistloaded': function() {
-          self.loadTrack(self.playlistIndex);
+          self.updatePlaylistUI();
+          self.changeTrack(self.playlistIndex);
         },
         'player-playpause': function() {
           if (self.$audio[0].paused === true) {
@@ -63,6 +75,7 @@
         },
       });
 
+      // UI triggers
       this.ui.bPlayPause.click(function(){
         self.$player.trigger('player-playpause');
       });
@@ -72,6 +85,13 @@
       this.ui.bNextTrack.click(function(){
         self.$player.trigger('player-nexttrack')
       });
+      this.ui.bMenuButton.click(function(){
+        self.$player.toggleClass('is-playlist-open');
+      });
+
+      this.ui.uPlaylist.on('click', 'li', function(){
+        self.changeTrack($(this).index());
+      })
     },
 
     loadPlaylist: function(tracklist) {
@@ -115,27 +135,66 @@
       this.ui.iTrackTitle.text(track['title']);
       this.ui.iArtistName.text(track['artist']);
       this.ui.iTotalTime.text(track['length']);
+      this.ui.iCoverArt.attr('src', track['cover']);
     },
     updateTimeUI: function() {
       var cur = this.$audio[0].currentTime;
       var sec = (cur % 60 < 10) ? ('0'+parseInt(cur % 60)) : parseInt(cur % 60);
       this.ui.iCurrentTime.text(parseInt(cur / 60)+':'+sec);
+
+      // Seeker
+      var pct = this.$audio[0].currentTime / this.$audio[0].duration * 100;
+      this.ui.uProgress.children('span').css('width', pct+'%');
+    },
+    updatePlaylistUI: function() {
+      var self = this;
+      var tracklist = this.ui.uPlaylist;
+
+      $.each(this.playlist, function(i){
+        var li   = document.createElement('li');
+        var a    = document.createElement('a');
+        var span = document.createElement('span');
+
+        a.innerHTML    = self.playlist[i]['title'];
+        a.setAttribute('class', 'no-ajaxy');
+        span.innerHTML = self.playlist[i]['length'];
+
+        a.appendChild(span);
+        li.appendChild(a);
+        tracklist.append(li);
+      });
     },
 
     prevTrack: function() {
       if (this.playlistIndex !== 0) {
         this.playlistIndex--;
-        this.loadTrack(this.playlistIndex);
+        this.changeTrack(this.playlistIndex);
       }
     },
     nextTrack: function() {
       if (this.playlist.length !== this.playlistIndex + 1) {
         this.playlistIndex++;
-        this.loadTrack(this.playlistIndex);
+        this.changeTrack(this.playlistIndex);
       }
     },
     changeTrack: function(index) {
       this.loadTrack(index);
+
+      // Active states for next/prev buttons
+      if (index === 0) {
+        this.ui.bPrevTrack.addClass('inactive');
+        this.ui.bNextTrack.removeClass('inactive')
+      }
+      if (index === this.playlist.length - 1) {
+        this.ui.bNextTrack.addClass('inactive')
+        this.ui.bPrevTrack.removeClass('inactive');
+      }
+      if (index === 0 && index === this.playlist.length - 1) {
+        this.ui.bPrevTrack.add(this.ui.bNextTrack).addClass('inactive');
+      }
+
+      // Active states for playlist
+      this.ui.uPlaylist.children().removeClass('active').eq(this.playlistIndex).addClass('active');
     }
   }
 
@@ -149,6 +208,7 @@
       var $parent = $(this).closest('[data-player-collection]');
       var $collection = $(this).closest('[data-player-collection]').find('[data-player-track]');
 
+      // No collection found, this is a standalone track
       if ($parent.length === 0) {
         masterPlayer.loadPlaylist($(this).attr('data-player-track'));
         return;
